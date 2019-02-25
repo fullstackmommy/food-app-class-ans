59fca59be0bc2ae0dcecc869e83d0b4068ba5bbe
Angeline <atan@thoughtworks.com>
2019-02-25 15:38:40 +0800

    Add card style to Restaurant

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Restaurant({item}) {
+function Restaurant({ item }) {
   const {
     _id,
     name,
@@ -10,12 +10,25 @@ function Restaurant({item}) {
     cuisine,
     imageUrl,
     averagePrice
-  } = item
+  } = item;
+
+  const cardStyle = { width: "18rem" };
+
   return (
-    <div>
-      id: {_id}
-      name: {name}
-      address: {address}
+    <div className="card" style={cardStyle}>
+      <img src={imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <div className="card-text">
+          <h6 className="text-muted">{cuisine.name}</h6>
+          <h6>
+            {openingTime} - {closingTime}
+          </h6>
+        </div>
+      </div>
+      <div className="card-footer text-muted">
+        <button className="btn btn-primary">Order</button>
+      </div>
     </div>
   );
 }

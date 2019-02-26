16eff47399f5c5b7a872406b6603f32f0f679723
Angeline <atan@thoughtworks.com>
2019-02-26 14:20:39 +0800

    Add filtering function for filterBar

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function FilterBar({cuisines}) {
+function FilterBar({cuisines, handleClick}) {
 
   return (
     <div className="btn-group" role="group" aria-label="Basic example">
       {cuisines.map(cuisine => (
-        <button key={cuisine._id} type="button" className="btn btn-outline-primary">
+        <button key={cuisine._id} type="button" className="btn btn-outline-primary" onClick={() => handleClick(cuisine.name)}>
           {cuisine.name}
         </button>
       ))}
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,19 +8,27 @@ import "./HomePage.scss";
 class HomePage extends Component {
   state = {
     restaurants: getRestaurants(),
-    cuisines: [getDefaultCuisine(), ...getCuisines()]
+    cuisines: [getDefaultCuisine(), ...getCuisines()],
+    selectedCuisine: getDefaultCuisine().name
   };
+
+  handleClick= (cuisineName) => {
+    this.setState({selectedCuisine : cuisineName})
+  }
+
   render() {
-    const { restaurants, cuisines } = this.state;
+    const { restaurants, cuisines, selectedCuisine } = this.state;
+    const renderRestaurants = selectedCuisine === getDefaultCuisine().name ? restaurants : restaurants.filter( res => res.cuisine.name === selectedCuisine )
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-4 mx-auto mt-3">
-            <FilterBar cuisines={cuisines}/>
+            <FilterBar cuisines={cuisines} handleClick={this.handleClick}/>
           </div>
         </div>
         <div className="row">
-          {restaurants.map(restaurant => (
+          {renderRestaurants.map(restaurant => (
             <div className="card-col" key={restaurant._id}>
               <Restaurant details={restaurant} />
             </div>

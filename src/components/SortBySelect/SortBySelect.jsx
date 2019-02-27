f345599c3bad9f757ea8f8eeb4e3df3cf7fff7f2
Angeline <atan@thoughtworks.com>
2019-02-27 14:51:35 +0800

    Create test to check sorting feature

--- src/components/SortBySelect/SortBySelect.jsx
+++ src/components/SortBySelect/SortBySelect.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 function SortBySelect({ handleSortSelect, selectedSort, selectOptions }) {
   return (
+    <label>
+      Sort By: 
+     </label> 
     <select
       className="btn btn-primary"
       onChange={handleSortSelect}
       value={selectedSort}
+      title="sort-select"
+      data-testid="sort-select"
     >
       {selectOptions.map((option, index) => (
         <option key={index} value={option.value}>

796daaa48a63b39967bc2ee30ae722050a44d46c
Angeline <atan@thoughtworks.com>
2019-02-28 15:28:58 +0800

    Add router test for Navbar

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function AdminPage() {
   return (
+    <div data-testid="admin-page">
     <h1>Admin Page</h1>
+    </div>
   )
 }
 
--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function OrderPage() {
   return (
+    <div data-testid="order-page">
     <h1>Order Page</h1>
+    </div>
   )
 }
 
